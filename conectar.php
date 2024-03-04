1a593f3622dfe69cfe2f5d60ef20e22aeb9070c7
davidpsousa <62971705+prodigyq@users.noreply.github.com>
2024-03-03 22:06:17 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -5,7 +5,7 @@ $bd = "cadastrar";
 $usuario = "root";
 $senha = "";
 
-//Criação de objeto abaixo
+//Criando a conexão com o banco de dados
 $con = new mysqli($hostname, $usuario, $senha, $bd);
 if ($con->connect_errno){
     echo "Erro de conexão com o banco de dados: (" . $con->connect_errno . ")" . $con->connect_error;
