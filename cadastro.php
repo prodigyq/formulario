b9f976664c399764009dccdf3490baf1522b0946
davidpsousa <62971705+prodigyq@users.noreply.github.com>
2024-03-03 22:09:08 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concluído</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
+<div>
 <?php
 
+//Incluindo o arquivo de conexão com o banco de dados e salvando os dados preenchidos pelo usuário 
 include("conectar.php");
 
 $nome = $_POST['nome'];
@@ -12,15 +26,17 @@ $idade = $diferença->y;
 $sexo = $_POST['sexo'];
 $justificativa = $_POST['justificativa'];
 
+//Inserindo os dados no banco de dados
 $sql = "INSERT INTO usuarios (nome, matricula, email, sexo, idade, justificativa) VALUES ('$nome', '$matricula', '$email', '$sexo', '$idade', '$justificativa')";
 
 $resultado = mysqli_query($con, $sql);
 
+//Verificando se os dados foram salvos no banco de dados
 if(mysqli_insert_id($con)){
-    echo "Conexão bem sucedida!";
-    echo "Seus dados foram salvos com sucesso!";
+    echo "Seus dados foram enviados com sucesso!";
 }
 else{
     echo "Erro na conexão!";
 }
 ?>
+</div>
